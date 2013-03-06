29ad13d1c1f57644a5288ced35767cea7010c00e
Philipp Borgers <borgers@mi.fu-berlin.de>
2013-03-06 03:21:04 +0100

    hide live rendering if mode is not markdown

--- public/javascripts/doc.js
+++ public/javascripts/doc.js
@@ -2,16 +2,19 @@ var converter = new Showdown.converter();
 var editor = ace.edit("editor");
 var elem = document.getElementById("markdown");
 
+var mode = 'markdown';
+var update = null;
+
 sharejs.open(id, 'text', function(error, doc){
   doc.attach_ace(editor);
-
-  doc.on('change', function(){
-    elem.innerHTML = converter.makeHtml(doc.snapshot);
-  });
-
-  elem.innerHTML = converter.makeHtml(doc.snapshot);
+  update = function(){
+    if(mode == 'markdown') {
+      elem.innerHTML = converter.makeHtml(doc.snapshot);
+    }
+  };
+  doc.on('change', update);
+  update();
 });
-
 
 $('.theme').click(function(){
   var theme = $(this).text();
@@ -19,6 +22,13 @@ $('.theme').click(function(){
 });
 
 $('.mode').click(function(){
-  var mode = $(this).text();
+  mode = $(this).text();
+  if(mode != 'markdown') {
+    console.log("hide");
+    $('#markdown').hide();
+  } else {
+    $('#markdown').show();
+    update();
+  }
   editor.getSession().setMode("ace/mode/" + mode);
 });
